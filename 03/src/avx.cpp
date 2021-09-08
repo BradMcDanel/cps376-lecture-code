b1fb0e6e25f1156955e1caaaae7dd7ba02caa6e2
Brad McDanel <mcdabw6@gmail.com>
2021-09-08 15:58:56 -0400

    Added example of parallel reduction using AVX-256.

---
+++ 03/src/avx.cpp
@@ -0,0 +1,55 @@
+#include <immintrin.h>
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+// Just to make it easier to read...
+template <typename T>
+std::chrono::microseconds micros(T duration) {
+  return std::chrono::duration_cast<std::chrono::microseconds>(duration);
+}
+
+float sum(const std::vector<float>& v) {
+  float sum = 0;
+  for (auto& x : v) {
+    sum += x;
+  }
+  return sum;
+}
+
+float sum_avx_256(const std::vector<float>& v) {
+  __m256 sum = _mm256_setzero_ps();
+  for (size_t i = 0; i < v.size(); i += 8) {
+    __m256 x = _mm256_loadu_ps(&v[i]);
+    sum = _mm256_add_ps(sum, x);
+  }
+
+  float result[8];
+  _mm256_storeu_ps(result, sum);
+  float final_sum = 0;
+  for (auto& x : result) {
+    final_sum += x;
+  }
+  return final_sum;
+}
+
+int main() {
+  std::vector<float> v(8 * 1024 * 1024);
+  for (size_t i = 0; i < v.size(); i++) {
+    v[i] = 1;
+  }
+
+  auto start = std::chrono::high_resolution_clock::now();
+  std::cout << "sum: " << sum(v) << std::endl;
+  auto end = std::chrono::high_resolution_clock::now();
+  std::cout << "sum time: " << micros(end - start).count() << " us"
+            << std::endl;
+
+  start = std::chrono::high_resolution_clock::now();
+  std::cout << "sum_avx: " << sum_avx_256(v) << std::endl;
+  end = std::chrono::high_resolution_clock::now();
+  std::cout << "sum_avx time: " << micros(end - start).count() << " us"
+            << std::endl;
+
+  return 0;
+}

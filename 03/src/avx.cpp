b32105674e3fc511394c513801601d3f0d9cf5da
Brad McDanel <mcdabw6@gmail.com>
2021-09-08 16:04:49 -0400

    Added speedup as part of the print out in the example. Also, switched to aligned AVX instructions.

--- 03/src/avx.cpp
+++ 03/src/avx.cpp
@@ -20,12 +20,12 @@ float sum(const std::vector<float>& v) {
 float sum_avx_256(const std::vector<float>& v) {
   __m256 sum = _mm256_setzero_ps();
   for (size_t i = 0; i < v.size(); i += 8) {
-    __m256 x = _mm256_loadu_ps(&v[i]);
+    __m256 x = _mm256_load_ps(&v[i]);
     sum = _mm256_add_ps(sum, x);
   }
 
   float result[8];
-  _mm256_storeu_ps(result, sum);
+  _mm256_store_ps(result, sum);
   float final_sum = 0;
   for (auto& x : result) {
     final_sum += x;
@@ -42,13 +42,20 @@ int main() {
   auto start = std::chrono::high_resolution_clock::now();
   std::cout << "sum: " << sum(v) << std::endl;
   auto end = std::chrono::high_resolution_clock::now();
-  std::cout << "sum time: " << micros(end - start).count() << " us"
-            << std::endl;
+  float serial_duration = micros(end - start).count();
 
   start = std::chrono::high_resolution_clock::now();
   std::cout << "sum_avx: " << sum_avx_256(v) << std::endl;
   end = std::chrono::high_resolution_clock::now();
+  float avx_duration = micros(end - start).count();
+
+  // In real benchmarching code, we would want run this many iterations but
+  // for simplicity, we just run it once.
+  std::cout << "sum time: " << micros(end - start).count() << " us"
+            << std::endl;
   std::cout << "sum_avx time: " << micros(end - start).count() << " us"
+            << std::endl;
+  std::cout << "speedup: " << serial_duration / avx_duration << "x"
             << std::endl;
 
   return 0;

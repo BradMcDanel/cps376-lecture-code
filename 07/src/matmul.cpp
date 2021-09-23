a390cb75ebb7c0590547c55315ccef5e50cdc146
Brad McDanel <bmcdanel@fandm.edu>
2021-09-23 13:33:13 -0400

    Updated lecture 7 code.

--- 07/src/matmul.cpp
+++ 07/src/matmul.cpp
@@ -1,16 +1,15 @@
-#include <omp.h>
 #include <chrono>
 #include <iostream>
+#include <omp.h>
 
 // Just to make it easier to read...
-template <typename T>
-std::chrono::microseconds micros(T duration) {
+template <typename T> std::chrono::microseconds micros(T duration) {
   return std::chrono::duration_cast<std::chrono::microseconds>(duration);
 }
 
 class Matrix {
- public:
-  int* d;
+public:
+  int *d;
   int rows;
   int cols;
 
@@ -24,7 +23,7 @@ class Matrix {
 
 // A perf comparison of serial matmul, serial tiled matmul, and
 // parallel matmul using omp.
-Matrix serial_matmul(const Matrix& A, const Matrix& B) {
+Matrix serial_matmul(const Matrix &A, const Matrix &B) {
   Matrix C(A.rows, B.cols);
   for (int i = 0; i < A.rows; i++) {
     for (int j = 0; j < B.cols; j++) {
@@ -37,7 +36,7 @@ Matrix serial_matmul(const Matrix& A, const Matrix& B) {
   return C;
 }
 
-Matrix serial_tiled_matmul(const Matrix& A, const Matrix& B, int tile_size) {
+Matrix serial_tiled_matmul(const Matrix &A, const Matrix &B, int tile_size) {
   Matrix C(A.rows, B.cols);
   int tile_rows = A.rows / tile_size;
   int tile_cols = B.cols / tile_size;
@@ -56,28 +55,7 @@ Matrix serial_tiled_matmul(const Matrix& A, const Matrix& B, int tile_size) {
   return C;
 }
 
-Matrix parallel_matmul(const Matrix& A, const Matrix& B, int tile_size) {
-  Matrix C(A.rows, B.cols);
-  int tile_rows = A.rows / tile_size;
-  int tile_cols = B.cols / tile_size;
-  int* C_d = C.d;
-
-#pragma omp parallel for reduction(+ : C_d)
-  for (int i = 0; i < tile_rows; i++) {
-    for (int j = 0; j < tile_cols; j++) {
-      for (int k = 0; k < tile_size; k++) {
-        for (int l = 0; l < tile_size; l++) {
-          C.d[i * B.cols + j] += A.d[i * A.cols + k * tile_size + l] *
-                                 B.d[l * B.cols + j * tile_size + k];
-        }
-      }
-    }
-  }
-
-  return C;
-}
-
-bool equals(const Matrix& A, const Matrix& B) {
+bool equals(const Matrix &A, const Matrix &B) {
   for (int i = 0; i < A.rows * A.cols; i++) {
     if (A.d[i] == B.d[i]) {
       return false;
@@ -115,14 +93,14 @@ int main() {
   std::cout << "serial tiled matmul took " << micros(end - start).count()
             << " us" << std::endl;
 
-  start = std::chrono::high_resolution_clock::now();
-  Matrix E = parallel_matmul(A, B, tile_size);
-  end = std::chrono::high_resolution_clock::now();
-  std::cout << "serial tiled matmul took " << micros(end - start).count()
-            << " us" << std::endl;
+  // start = std::chrono::high_resolution_clock::now();
+  // Matrix E = parallel_matmul(A, B, tile_size);
+  // end = std::chrono::high_resolution_clock::now();
+  // std::cout << "serial tiled matmul took " << micros(end - start).count()
+  //           << " us" << std::endl;
 
-  std::cout << "Results are the same? " << equals(C, D) << std::endl;
-  std::cout << "Results are the same? " << equals(C, E) << std::endl;
+  // std::cout << "Results are the same? " << equals(C, D) << std::endl;
+  // std::cout << "Results are the same? " << equals(C, E) << std::endl;
 
   return 0;
 }

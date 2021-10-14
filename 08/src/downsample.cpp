a083eb9e2d2ded8e8eb7099a5d90d5db3b46baa4
Brad McDanel <mcdabw6@gmail.com>
2021-10-14 19:57:40 -0400

    Added downsample example

--- 08/src/downsample.cpp
+++ 08/src/downsample.cpp
@@ -4,11 +4,19 @@
 #include "image.h"
 
 int main() {
-  int scale = 4;
+  int scale = 32;
   Image img("../data/fm.png");
   Image out_img(img.w / scale, img.h / scale);
 
-  // Goal: Use OpenMP to downsample the image by a factor of scale (4x).
+// downsamples the image by a factor of scale (32x here) using OpenMP
+#pragma omp parallel for collapse(2)
+  for (int i = 0; i < out_img.h; i++) {
+    for (int j = 0; j < out_img.w; j++) {
+      out_img.set(i, j, img.get(i * scale, j * scale));
+    }
+  }
+
+  out_img.write("../data/fm_out.png");
 
   return 0;
 }
